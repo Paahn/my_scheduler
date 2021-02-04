a9da9637c29feca051746be520b06f2765799a7f
Paahn <paahndev@gmail.com>
2021-02-03 20:21:42 -0800

    schema for event model

---
+++ models/event.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    time: {
+        type: String,
+        required: true,
+    }
+})
