371457523382596c3a398df4c3e0545d76da77c7
Paahn <paahndev@gmail.com>
2021-02-04 16:42:14 -0800

    updated query to retrieve the events query with the MongoDB object now
    we return event._doc as we are filtering out the mongoDB meta data, to get just the data we need

--- app.js
+++ app.js
@@ -47,7 +47,16 @@ app.use('/graphql', graphqlHTTP({
     `),
     rootValue: {
         events: () => {
-            return events;
+            return Event
+            .find()
+            .then(events => {
+                return events.map(event => {
+                    return {...event._doc};
+                });
+            })
+            .catch(err => {
+                throw err;
+            });
         },
         createEvent: ({eventInput}) => {
             // const event = {
